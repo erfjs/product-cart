4f8f3e0ed5c90fc5907e61c30a916447e4169cd8
erfii <erfan.granmayeh@gmail.com>
2022-07-01 14:58:25 +0430

    dependences

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,93 +1,98 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from 'react'
+import Gradients from '../components/Gradients'
+import ProductImages from '../components/ProductImages'
+import Info from '../components/Info/Info'
 
-import Gradients from "../components/Gradients";
-import ProductImages from "../components/ProductImages";
-import Info from "../components/Info/Info";
-
-import logo from "../assets/img/logo.png";
+import logo from '../assets/img/logo.png'
 
 const Home = () => {
-  var sizes, colors, shoes, gradients, shoeBackground, shoeHeight;
-  var prevColor = "blue";
-  var animateOrNot = true;
+  var shoeHeight
+  var prevColor = 'blue'
+  var animateOrNot = true
+
+  const colors = useRef(null)
+  const sizes = useRef(null)
+  const shoes = useRef(null)
+  const gradients = useRef(null)
+  const shoeBackground = useRef(null)
 
   function changeColor() {
     if (!animateOrNot) {
-      console.log("waittttt");
-      return;
+      console.log('waittttt')
+      return
     }
-    var primary = this.getAttribute("primary");
-    var color = this.getAttribute("color");
-    var shoe = document.querySelector(`.shoe[color="${color}"]`);
-    var gradient = document.querySelector(`.gradient[color="${color}"]`);
-    var prevGradient = document.querySelector(
-      `.gradient[color="${prevColor}"]`
-    );
+    var primary = this.getAttribute('primary')
+    var color = this.getAttribute('color')
+    var shoe = document.querySelector(`.shoe[color="${color}"]`)
+    var gradient = document.querySelector(`.gradient[color="${color}"]`)
+    var prevGradient = document.querySelector(`.gradient[color="${prevColor}"]`)
 
     // showing correct color
-    colors.forEach((color) => color.classList.remove("active"));
-    this.classList.add("active");
+    colors.current.forEach(color => color.classList.remove('active'))
+    this.classList.add('active')
 
     // changing primary css variable
-    document.documentElement.style.setProperty("--primary", primary);
+    document.documentElement.style.setProperty('--primary', primary)
 
     // showing correct img
-    shoes.forEach((s) => s.classList.remove("show"));
-    shoe.classList.add("show");
+    shoes.current.forEach(s => s.classList.remove('show'))
+    shoe.classList.add('show')
 
     // dealing with gradient
-    gradients.forEach((g) => g.classList.remove("display", "behind"));
-    prevGradient.classList.add("behind");
-    gradient.classList.add("display");
+    gradients.current.forEach(g => g.classList.remove('display', 'behind'))
+    prevGradient.classList.add('behind')
+    gradient.classList.add('display')
 
     // logic
-    prevColor = color;
-    animateOrNot = false;
+    prevColor = color
+    animateOrNot = false
 
     // hack
     setTimeout(() => {
-      animateOrNot = true;
-    }, 800);
+      animateOrNot = true
+    }, 800)
   }
 
   function changeSize() {
-    sizes.forEach((size) => size.classList.remove("active"));
-    this.classList.add("active");
+    sizes.current.forEach(size => size.classList.remove('active'))
+    this.classList.add('active')
   }
 
   // for responsive behaviour
   const changeHeight = () => {
-    var x = window.matchMedia("(max-width:1000px)");
-
-    !shoes ? (shoeHeight = 0) : (shoeHeight = shoes[0].offsetHeight);
+    var x = window.matchMedia('(max-width:1000px)')
+    !shoes.current ? (shoeHeight = 0) : (shoeHeight = shoes.current[0].offsetHeight)
 
     if (x.matches) {
       if (shoeHeight === 0) {
         try {
-          setTimeout(changeHeight, 50);
+          setTimeout(changeHeight, 50)
         } catch (error) {
-          alert("Something is Wrong!!");
+          alert('Something is Wrong!!')
         }
       }
-      shoeBackground.style.height = `${shoeHeight * 0.9}px`;
-    } else if (!!shoeBackground) {
+      shoeBackground.current.style.height = `${shoeHeight * 0.9}px`
+    } else if (!!shoeBackground.current) {
       // go back to default
-      shoeBackground.style.height = "475px";
+      shoeBackground.current.style.height = '475px'
     }
-  };
+  }
 
   useEffect(() => {
-    sizes = document.querySelectorAll(".size");
-    colors = document.querySelectorAll(".color");
-    shoes = document.querySelectorAll(".shoe");
-    gradients = document.querySelectorAll(".gradient");
-    shoeBackground = document.querySelector(".shoeBackground");
+    sizes.current = document.querySelectorAll('.size')
+    colors.current = document.querySelectorAll('.color')
+    shoes.current = document.querySelectorAll('.shoe')
+    gradients.current = document.querySelectorAll('.gradient')
+    shoeBackground.current = document.querySelector('.shoeBackground')
 
-    colors.forEach((color) => color.addEventListener("click", changeColor));
-    sizes.forEach((size) => size.addEventListener("click", changeSize));
-    changeHeight();
-  }, []);
-  window.addEventListener("resize", changeHeight);
+    colors.current.forEach(color => color.addEventListener('click', changeColor))
+
+    sizes.current.forEach(size => size.addEventListener('click', changeSize))
+
+    changeHeight()
+  }, [])
+
+  window.addEventListener('resize', changeHeight)
 
   return (
     <div className='Home'>
@@ -108,7 +113,7 @@ const Home = () => {
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
